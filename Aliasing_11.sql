ba1dc6dd034fbf816a33f3c7b84bf11733260a03
John <john.w.wojtalewicz@gmail.com>
2026-07-12 15:39:48 -0800

    Added examples of aliasing column names in queries.

---
+++ Aliasing_11.sql
@@ -0,0 +1,10 @@
+#Aliasing
+
+#Example showing aliasing column names in results with and without the as keyword
+select product_name as 'Goodie Name', units_in_stock 'UIS'
+from products;
+
+#Example of aliasing an equation in a query (common practice)
+select product_id 'Product ID', units_in_stock * sale_price 'Potential Revenue'
+from products;
+
