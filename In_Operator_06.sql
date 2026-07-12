3fce5a1d0687ba44cb5f1753005a28bdb40300ad
John <john.w.wojtalewicz@gmail.com>
2026-07-12 14:33:34 -0800

    Added additional examples of in and not in operators.

--- In_Operator_06.sql
+++ In_Operator_06.sql
@@ -4,3 +4,13 @@
 select *
 from customers
 where state in('CA', 'PA', 'TX', 'IL');
+
+#Second example
+select *
+from customers
+where first_name in('Kevin', 'Kelly', 'Frodo');
+
+#Example of not in operator.
+select *
+from customers
+where first_name not in('Kevin', 'Kelly', 'Frodo');
