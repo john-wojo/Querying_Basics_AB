54a19e75bb18f6047b9808e03d135d80461f17d1
John <john.w.wojtalewicz@gmail.com>
2026-07-12 12:20:08 -0800

    Checking for specific last names using where and in statements.

--- Select_Statement_02.sql
+++ Select_Statement_02.sql
@@ -1,5 +1,7 @@
 #Looking at who spent how much money in what city.
 #In descending order of who spent the most.
+#Checking for specific last names using where and in statements
 SELECT first_name, last_name, city, total_money_spent
 FROM customers
-ORDER BY total_money_spent DESC;
+WHERE last_name IN('Kenobi', 'Malone')
+ORDER BY total_money_spent desc;
