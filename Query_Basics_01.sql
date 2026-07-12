a92a729c3f8d8d484446e078a3449d7ce432859b
John <john.w.wojtalewicz@gmail.com>
2026-07-12 11:52:32 -0800

    Edited query to look at customer_orders table with the product_id = 1001.

--- Query_Basics_01.sql
+++ Query_Basics_01.sql
@@ -7,5 +7,8 @@
 
 #Final option is to double click on the database in MySQL
 
+#Now selecting all rows where from the customer_order table 
+#where the product_id is equal to 1001.
 SELECT *
-FROM customer_orders;
+FROM customer_orders
+WHERE product_id = 1001;
