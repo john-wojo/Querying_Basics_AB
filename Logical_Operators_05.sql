fa634a2ac766cc31714d4bf149f60a83710d3b11
John <john.w.wojtalewicz@gmail.com>
2026-07-12 14:03:03 -0800

    Used OR logical operator in an example.,

--- Logical_Operators_05.sql
+++ Logical_Operators_05.sql
@@ -1,6 +1,12 @@
 #Logical Operators
 # and, or, not
 
+#Using AND logical operator to find customers who are in pensylvania and also spent over 1,000 
 select *
 from customers
 where state = 'PA' and total_money_spent > 1000;
+
+#Using OR logical operator to find customers who are in pensylvania or have spent over 1,000
+select *
+from customers
+where state = 'PA' or total_money_spent > 1000;
