9e772ef64b202dceceef76d72b5212857db9bab9
John <john.w.wojtalewicz@gmail.com>
2026-07-12 13:40:21 -0800

    Additional changes, looking at low stock products, customers born before/after a specific date.

--- Where_Clause_03.sql
+++ Where_Clause_03.sql
@@ -4,3 +4,13 @@
 SELECT * 
 FROM customers
 WHERE city = 'Scranton';
+
+#Looking for customers born in or after 1985.
+SELECT *
+FROM customers
+WHERE birth_date > '1985-01-01';
+
+#Looking at products table for products that are low in stock
+select *
+from products
+where units_in_stock < 30;
