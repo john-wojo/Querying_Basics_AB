46dce8a14e5834a86d43b77e39ceb82b2d94ef62
John <john.w.wojtalewicz@gmail.com>
2026-07-12 11:45:59 -0800

    Selecting everything from customer_orders.

--- Query_Basics_01.sql
+++ Query_Basics_01.sql
@@ -2,7 +2,10 @@
 #USE bakery;
 
 #Can also use bakery.customer to specify database
-SELECT customer_id, first_name 
-FROM bakery.customers;
+#SELECT customer_id, first_name 
+#FROM bakery.customers;
 
 #Final option is to double click on the database in MySQL
+
+SELECT *
+FROM customer_orders;
