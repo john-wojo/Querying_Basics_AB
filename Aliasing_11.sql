6fc17437785513cf98121664f832531cd22fd97c
John <john.w.wojtalewicz@gmail.com>
2026-07-12 15:41:30 -0800

    Small change to final query from product id to product name to make it clearer.

--- Aliasing_11.sql
+++ Aliasing_11.sql
@@ -5,6 +5,6 @@ select product_name as 'Goodie Name', units_in_stock 'UIS'
 from products;
 
 #Example of aliasing an equation in a query (common practice)
-select product_id 'Product ID', units_in_stock * sale_price 'Potential Revenue'
+select product_name 'Product Name', units_in_stock * sale_price 'Potential Revenue'
 from products;
 
