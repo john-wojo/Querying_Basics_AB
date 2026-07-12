3d4a2a46439d74c54053c3fb659b20919c39bd45
John <john.w.wojtalewicz@gmail.com>
2026-07-12 11:35:59 -0800

    Querying Basics Analyst Builder

---
+++ Query_Basics_01.sql
@@ -0,0 +1,8 @@
+#USE allows you to specify the database before running another query.
+#USE bakery;
+
+#Can also use bakery.customer to specify database
+SELECT * 
+FROM bakery.customers;
+
+#Final option is to double click on the database in MySQL
